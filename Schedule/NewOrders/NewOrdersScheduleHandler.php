08d495ab2d7b92fe69bdd6b9aa676802209ff1e9
kingstar <admin@baks.dev>
2024-10-27 13:29:30 +0300

    Baks Development

--- Schedule/NewOrders/NewOrdersScheduleHandler.php
+++ Schedule/NewOrders/NewOrdersScheduleHandler.php
@@ -1,17 +1,17 @@
 <?php
 /*
- *  Copyright 2023.  Baks.dev <admin@baks.dev>
- *
+ *  Copyright 2024.  Baks.dev <admin@baks.dev>
+ *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
  *  in the Software without restriction, including without limitation the rights
  *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  *  copies of the Software, and to permit persons to whom the Software is furnished
  *  to do so, subject to the following conditions:
- *
+ *  
  *  The above copyright notice and this permission notice shall be included in all
  *  copies or substantial portions of the Software.
- *
+ *  
  *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,10 +25,12 @@ declare(strict_types=1);
 
 namespace BaksDev\Ozon\Orders\Schedule\NewOrders;
 
+use BaksDev\Core\Messenger\MessageDelay;
 use BaksDev\Core\Messenger\MessageDispatchInterface;
 use BaksDev\Ozon\Orders\Messenger\Schedules\NewOrders\NewOzonOrdersScheduleMessage;
 use BaksDev\Ozon\Repository\AllProfileToken\AllProfileOzonTokenInterface;
-use BaksDev\Users\Profile\UserProfile\Repository\UserByUserProfile\UserByUserProfileInterface;
+use DateInterval;
+use Random\Randomizer;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 
 #[AsMessageHandler]
@@ -36,7 +38,6 @@ final readonly class NewOrdersScheduleHandler
 {
     public function __construct(
         private AllProfileOzonTokenInterface $allProfileToken,
-        private UserByUserProfileInterface $userByUserProfile,
         private MessageDispatchInterface $messageDispatch,
     ) {}
 
@@ -49,19 +50,17 @@ final readonly class NewOrdersScheduleHandler
 
         if($profiles->valid())
         {
+            $Randomizer = new Randomizer();
+
             foreach($profiles as $profile)
             {
-                /** Получаем идентификатор пользователя */
-                $User = $this->userByUserProfile->forProfile($profile)->findUser();
+                $delay = sprintf('%s seconds', $Randomizer->getInt(5, 30));
 
-                if($User)
-                {
-                    $this->messageDispatch->dispatch(
-                        message: new NewOzonOrdersScheduleMessage($profile),
-                        transport: (string) $profile,
-                    );
-                }
-
+                $this->messageDispatch->dispatch(
+                    message: new NewOzonOrdersScheduleMessage($profile),
+                    stamps: [new MessageDelay(DateInterval::createFromDateString($delay))],
+                    transport: (string) $profile,
+                );
             }
         }
     }
