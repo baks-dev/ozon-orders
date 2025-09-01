5508ad0fa27314ef38b6a2c2aa2b37bdffe2b437
MichelAngelo <baksdevelopmen@gmail.com>
2025-09-01 21:22:06 +0300

    BaksDevelopment

--- Messenger/Package/UpdatePackageOzonOrderFbsDispatcher.php
+++ Messenger/Package/UpdatePackageOzonOrderFbsDispatcher.php
@@ -19,7 +19,6 @@
  *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  *  THE SOFTWARE.
- *
  */
 
 declare(strict_types=1);
@@ -355,6 +354,7 @@ final class UpdatePackageOzonOrderFbsDispatcher
 
         /**
          * Делаем запрос на разделение
+         *
          * @var $postings UpdateOzonOrdersPackageDTO|false
          */
         $postings = $this
@@ -413,7 +413,7 @@ final class UpdatePackageOzonOrderFbsDispatcher
                 ),
                 context: [
                     self::class.':'.__LINE__,
-                    var_export($postings, true)
+                    var_export($postings, true),
                 ],
             );
 
