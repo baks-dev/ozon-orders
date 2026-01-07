c8467607b886499e062afc43968b537dfba7f79b
MichelAngelo <baksdevelopmen@gmail.com>
2026-01-07 21:19:10 +0300

    BaksDevelopment

--- Messenger/TaskOzonPackageStickers/Create/CreateTaskOzonStickersDispatcher.php
+++ Messenger/TaskOzonPackageStickers/Create/CreateTaskOzonStickersDispatcher.php
@@ -1,6 +1,6 @@
 <?php
 /*
- *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
  *  
  *  Permission is hereby granted, free of charge, to any person obtaining a copy
  *  of this software and associated documentation files (the "Software"), to deal
@@ -88,7 +88,7 @@ final readonly class CreateTaskOzonStickersDispatcher
 
 
         /**
-         * Пробуем получить результат задания для стикера маркировки заказа
+         * Пробуем получить результат задания для стикера маркировки заказа через время
          */
 
         $TaskOzonPackageStickersMessage = new TaskOzonPackageStickersMessage(
