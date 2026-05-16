7e71640e41f18123e7423a9e380ad9c70ff624bd
MichelAngelo <baksdevelopmen@gmail.com>
2026-05-16 15:07:15 +0300

    BaksDevelopment

---
+++ Api/Exemplar/Tests/UpdateOzonOrdersExemplarRequestTest.php
@@ -0,0 +1,81 @@
+<?php
+/*
+ *  Copyright 2025.  Baks.dev <admin@baks.dev>
+ *  
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *  
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *  
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Ozon\Orders\Api\Exemplar\Tests;
+
+use BaksDev\Orders\Order\UseCase\Admin\Edit\Tests\OrderNewTest;
+use BaksDev\Ozon\Orders\Api\Exemplar\UpdateOzonOrdersExemplarRequest;
+use BaksDev\Ozon\Orders\Type\ProfileType\TypeProfileFbsOzon;
+use BaksDev\Ozon\Type\Authorization\OzonAuthorizationToken;
+use BaksDev\Products\Stocks\UseCase\Admin\Package\Tests\PackageProductStockTest;
+use BaksDev\Users\Profile\UserProfile\Type\Id\UserProfileUid;
+use BaksDev\Users\Profile\UserProfile\UseCase\User\NewEdit\Tests\UserNewUserProfileHandleTest;
+use PHPUnit\Framework\Attributes\Group;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\DependencyInjection\Attribute\When;
+
+#[Group('ozon-orders')]
+#[When(env: 'test')]
+class UpdateOzonOrdersExemplarRequestTest extends KernelTestCase
+{
+    private static OzonAuthorizationToken $Authorization;
+
+    public static function setUpBeforeClass(): void
+    {
+        OrderNewTest::setUpBeforeClass();
+        PackageProductStockTest::setUpBeforeClass();
+        UserNewUserProfileHandleTest::setUpBeforeClass();
+
+        self::$Authorization = new OzonAuthorizationToken(
+            new UserProfileUid('018d464d-c67a-7285-8192-7235b0510924'),
+            $_SERVER['TEST_OZON_TOKEN'],
+            TypeProfileFbsOzon::TYPE,
+            $_SERVER['TEST_OZON_CLIENT'],
+            $_SERVER['TEST_OZON_WAREHOUSE'],
+            '10',
+            0,
+            false,
+            false,
+        );
+    }
+
+    public function testUseCase(): void
+    {
+        self::assertTrue(true);
+
+        /** @var UpdateOzonOrdersExemplarRequest $UpdateOzonOrdersExemplarRequest */
+        $UpdateOzonOrdersExemplarRequest = self::getContainer()->get(UpdateOzonOrdersExemplarRequest::class);
+        $UpdateOzonOrdersExemplarRequest->TokenHttpClient(self::$Authorization);
+
+        $isUpdate = $UpdateOzonOrdersExemplarRequest
+            ->posting('O-00000000000-111111-1')
+            ->product('11111111111')
+            ->exemplar('222222222222')
+            ->sign("sign")
+            ->update();
+
+        self::assertIsBool($isUpdate);
+    }
+}
