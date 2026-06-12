379254ec54c743b7c97389564662a0884a64f744
MichelAngelo <baksdevelopmen@gmail.com>
2026-06-12 17:43:15 +0300

    BaksDevelopment

---
+++ Messenger/Dashboard/DashboardDispatcher.php
@@ -0,0 +1,204 @@
+<?php
+/*
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Ozon\Orders\Messenger\Dashboard;
+
+
+use BaksDev\Core\Deduplicator\DeduplicatorInterface;
+use BaksDev\Core\Messenger\MessageDelay;
+use BaksDev\Core\Messenger\MessageDispatchInterface;
+use BaksDev\Finances\Entity\Event\FinancesEvent;
+use BaksDev\Finances\Messenger\Default\FinancesMessage;
+use BaksDev\Finances\Repository\CurrentFinancesEvent\CurrentFinancesEventInterface;
+use BaksDev\Finances\Repository\Statistics\Orders\StatisticsOrdersInterface;
+use BaksDev\Finances\Repository\Statistics\Orders\StatisticsOrdersResult;
+use BaksDev\Finances\Repository\Statistics\Orders\Tests\StatisticsOrdersInterfaceTest;
+use BaksDev\Ozon\Orders\Messenger\Dashboard\CacheAll\DashboardCacheAllDayMessage;
+use BaksDev\Ozon\Orders\Messenger\Dashboard\CacheOrders\DashboardCacheOrdersDayMessage;
+use BaksDev\Ozon\Orders\Messenger\Dashboard\CacheOthers\DashboardCacheOthersDayMessage;
+use BaksDev\Ozon\Orders\Messenger\Dashboard\HoldAll\DashboardHoldAllDayMessage;
+use BaksDev\Ozon\Orders\Messenger\Dashboard\HoldOrders\DashboardHoldOrdersDayMessage;
+use BaksDev\Ozon\Orders\Messenger\Dashboard\HoldOthers\DashboardHoldOthersDayMessage;
+use BaksDev\Users\User\Type\Id\UserUid;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use Symfony\Component\DependencyInjection\Attribute\Target;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+/** Обновляет финансовые выплаты */
+#[Autoconfigure(shared: false)]
+#[AsMessageHandler(priority: 0)]
+final class DashboardDispatcher
+{
+    public function __construct(
+        #[Target('ozonOrdersLogger')] private LoggerInterface $logger,
+        private readonly CurrentFinancesEventInterface $CurrentFinancesEventRepository,
+        private readonly MessageDispatchInterface $dispatch,
+        private readonly DeduplicatorInterface $Deduplicator,
+        private readonly StatisticsOrdersInterface $StatisticsOrdersRepository,
+    ) {}
+
+    public function __invoke(FinancesMessage $message): void
+    {
+
+        /** Получаем информацию о платеже */
+
+        $FinancesEvent = $this->CurrentFinancesEventRepository
+            ->forFinanceMain($message->getId())
+            ->find();
+
+        if(false === ($FinancesEvent instanceof FinancesEvent))
+        {
+            $this->logger->critical(
+                'finances: Ошибка при получении финансовой выплаты',
+                [self::class.':'.__LINE__],
+            );
+
+            return;
+        }
+
+        if(
+            false === $FinancesEvent->isInvariable()
+            || false === $FinancesEvent->isPayment()
+        )
+        {
+            return;
+        }
+
+        $UserUid = $FinancesEvent->getInvariable()->getUserFinance();
+
+        /** Дедубликатор по идентификатору заказа */
+        $Deduplicator = $this->Deduplicator
+            ->namespace('orders-order')
+            ->deduplication([
+                (string) $UserUid,
+                self::class,
+            ]);
+
+        if($Deduplicator->isExecuted() === true)
+        {
+            return;
+        }
+
+        if(true === $FinancesEvent->isOrders())
+        {
+            /**
+             * Создаем доску "Выплаты по заказам"
+             */
+
+            $DashboardCacheOrdersDayMessage = new DashboardCacheOrdersDayMessage(
+                payment: $FinancesEvent->getPayment(),
+                user: $UserUid,
+            );
+
+            $this->dispatch->dispatch(
+                message: $DashboardCacheOrdersDayMessage,
+                stamps: [new MessageDelay('30 minutes')],
+                transport: 'ozon-orders',
+            );
+
+
+            /**
+             * Создаем доску "Удержано по заказам"
+             */
+
+            $DashboardHoldOrdersDayMessage = new DashboardHoldOrdersDayMessage(
+                payment: $FinancesEvent->getPayment(),
+                user: $UserUid,
+            );
+
+            $this->dispatch->dispatch(
+                message: $DashboardHoldOrdersDayMessage,
+                stamps: [new MessageDelay('31 minutes')],
+                transport: 'ozon-orders',
+            );
+        }
+
+        if(false === $FinancesEvent->isOrders())
+        {
+            /**
+             * Создаем доску "Прочие выплаты"
+             */
+
+            $DashboardCacheOthersDayMessage = new DashboardCacheOthersDayMessage(
+                payment: $FinancesEvent->getPayment(),
+                user: $UserUid,
+            );
+
+            $this->dispatch->dispatch(
+                message: $DashboardCacheOthersDayMessage,
+                stamps: [new MessageDelay('35 minutes')],
+                transport: 'ozon-orders',
+            );
+
+            /**
+             * Создаем доску "Прочих удержаний"
+             */
+
+            $DashboardHoldOthersDayMessage = new DashboardHoldOthersDayMessage(
+                payment: $FinancesEvent->getPayment(),
+                user: $UserUid,
+            );
+
+            $this->dispatch->dispatch(
+                message: $DashboardHoldOthersDayMessage,
+                stamps: [new MessageDelay('36 minutes')],
+                transport: 'ozon-orders',
+            );
+        }
+
+
+        /**
+         * Создаем доску "Итого выплат"
+         */
+
+        $DashboardCacheAllDayMessage = new DashboardCacheAllDayMessage(
+            payment: $FinancesEvent->getPayment(),
+            user: $UserUid,
+        );
+
+        $this->dispatch->dispatch(
+            message: $DashboardCacheAllDayMessage,
+            stamps: [new MessageDelay('40 minutes')],
+            transport: 'ozon-orders',
+        );
+
+
+        /**
+         * Создаем доску "Всего удержано"
+         */
+
+        $DashboardHoldAllDayMessage = new DashboardHoldAllDayMessage(
+            payment: $FinancesEvent->getPayment(),
+            user: $UserUid,
+        );
+
+        $this->dispatch->dispatch(
+            message: $DashboardHoldAllDayMessage,
+            stamps: [new MessageDelay('41 minutes')],
+            transport: 'ozon-orders',
+        );
+    }
+}
---
+++ Messenger/Dashboard/HoldOrders/DashboardHoldOrdersDayDispatcher.php
@@ -0,0 +1,155 @@
+<?php
+/*
+ *  Copyright 2026.  Baks.dev <admin@baks.dev>
+ *
+ *  Permission is hereby granted, free of charge, to any person obtaining a copy
+ *  of this software and associated documentation files (the "Software"), to deal
+ *  in the Software without restriction, including without limitation the rights
+ *  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ *  copies of the Software, and to permit persons to whom the Software is furnished
+ *  to do so, subject to the following conditions:
+ *
+ *  The above copyright notice and this permission notice shall be included in all
+ *  copies or substantial portions of the Software.
+ *
+ *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ *  FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE
+ *  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ *  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ *  THE SOFTWARE.
+ */
+
+declare(strict_types=1);
+
+namespace BaksDev\Ozon\Orders\Messenger\Dashboard\HoldOrders;
+
+
+use BaksDev\Core\Deduplicator\DeduplicatorInterface;
+use BaksDev\Core\Messenger\MessageDispatchInterface;
+use BaksDev\Dashboard\Entity\Dashboard;
+use BaksDev\Dashboard\Entity\Event\DashboardEvent;
+use BaksDev\Dashboard\Repository\DashboardCurrentEventByPeriod\DashboardCurrentEventByPeriodInterface;
+use BaksDev\Dashboard\UseCase\NewEdit\NewEditDashboardDTO;
+use BaksDev\Dashboard\UseCase\NewEdit\NewEditDashboardHandler;
+use BaksDev\Dashboard\UseCase\NewEdit\Type\NewEditDashboardTypeDTO;
+use BaksDev\Finances\Repository\CurrentFinancesEvent\CurrentFinancesEventInterface;
+use BaksDev\Finances\Repository\Statistics\Orders\StatisticsOrdersInterface;
+use BaksDev\Finances\Repository\Statistics\Orders\StatisticsOrdersResult;
+use BaksDev\Payment\Type\Id\PaymentUid;
+use BaksDev\Reference\Money\Type\Money;
+use BaksDev\Users\User\Type\Id\UserUid;
+use DateInterval;
+use DateTimeImmutable;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autoconfigure;
+use Symfony\Component\DependencyInjection\Attribute\Target;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[Autoconfigure(shared: false)]
+#[AsMessageHandler(priority: 0)]
+final class DashboardHoldOrdersDayDispatcher
+{
+    public function __construct(
+        #[Target('ozonOrdersLogger')] private LoggerInterface $logger,
+        private readonly DeduplicatorInterface $Deduplicator,
+        private readonly StatisticsOrdersInterface $StatisticsOrdersRepository,
+        private readonly NewEditDashboardHandler $NewEditDashboardHandler,
+        private readonly DashboardCurrentEventByPeriodInterface $DashboardCurrentEventByPeriodRepository,
+    ) {}
+
+    public function __invoke(DashboardHoldOrdersDayMessage $message): void
+    {
+
+        /** Дедубликатор по идентификатору заказа */
+        $Deduplicator = $this->Deduplicator
+            ->namespace('ozon-orders')
+            ->expiresAfter('1 hour')
+            ->deduplication([
+                $message,
+                self::class,
+            ]);
+
+        if($Deduplicator->isExecuted() === true)
+        {
+            return;
+        }
+
+        /** Получаем положительные транзакции по заказу за сутки */
+
+        $dayFrom = new DateTimeImmutable('now')->sub(DateInterval::createFromDateString('1 day'));
+        $dayTo = new DateTimeImmutable('now')->sub(DateInterval::createFromDateString('1 day'));
+
+        $StatisticsOrdersResult = $this
+            ->StatisticsOrdersRepository
+            ->forUser($message->getUser())
+            ->forPayment($message->getPayment())
+            ->onlyOrders() // только транзакции по заказу
+            //->onlyNotOrders() // только транзакции не имеющие заказы
+            ->onlyHold() // только отрицательный баланс
+            ->dayFrom($dayFrom)
+            ->dayTo($dayTo)
+            ->find();
+
+        if(false === ($StatisticsOrdersResult instanceof StatisticsOrdersResult))
+        {
+            return;
+        }
+
+
+        /** Создаем Dashboard на вчерашний день, чтобы отобразить сегодня */
+
+        // получаем имеющийся Dashboard
+
+
+        /** @see DashboardDTO */
+        $NewEditDashboardDTO = new NewEditDashboardDTO();
+
+        $DashboardEvent = $this->DashboardCurrentEventByPeriodRepository
+            ->user($message->getUser())
+            ->payment($message->getPayment())
+            ->period($dayFrom, $dayTo)
+            ->type('hold_orders_day')
+            ->find();
+
+        if(true === $DashboardEvent instanceof DashboardEvent)
+        {
+            $DashboardEvent->getDto($NewEditDashboardDTO);
+        }
+
+        /** Создаем новый объект */
+        if(false === $DashboardEvent instanceof DashboardEvent)
+        {
+            $DashboardInvariableDTO = $NewEditDashboardDTO->getInvariable();
+            $DashboardInvariableDTO
+                ->setName('Удержано по заказам')
+                ->setPeriod($dayFrom, $dayTo);
+
+            $NewEditDashboardTypeDTO = $NewEditDashboardDTO->getType();
+            $NewEditDashboardTypeDTO->setValue('hold_orders_day');
+
+            $DashboardPaymentDTO = $NewEditDashboardDTO->getPayment();
+            $DashboardPaymentDTO->setValue($message->getPayment());
+
+            $DashboardUserDTO = $NewEditDashboardDTO->getUser();
+            $DashboardUserDTO->setValue($message->getUser());
+        }
+
+        $NewEditDashboardDTO->setTotal($StatisticsOrdersResult->getTotal());
+
+        $Dashboard = $this->NewEditDashboardHandler->handle($NewEditDashboardDTO);
+
+        if(false === ($Dashboard instanceof Dashboard))
+        {
+            $this->logger->critical(
+                'finances: Ошибка при создании Dashboard "Выплаты по заказам"',
+                [self::class.':'.__LINE__],
+            );
+
+            return;
+        }
+
+        $Deduplicator->save();
+    }
+}
